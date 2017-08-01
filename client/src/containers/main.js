b1ea641b6363b81f1586f6a666f24cee9a66fce9
Peter Tan <pxhtan@gmail.com>
2017-08-01 14:45:43 -0700

    Fix main.js

--- client/src/containers/main.js
+++ client/src/containers/main.js
@@ -7,6 +7,7 @@ import { Segment } from 'semantic-ui-react';
 import Groups from './groups';
 import Channels from './channels';
 import Messages from './messages';
+
 import Events from './events/events';
 import CreateEvent from './events/createEvent';
 import EventDetails from './events/eventDetails';
